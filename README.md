03aa40d610dd4b623ddedbb890a6ed20005194df
Uiharu Kazari <xucgphi@gmail.com>
2018-07-17 23:09:30 -0400

    First commit:Adding files.

---
+++ README.md
@@ -0,0 +1,7 @@
+#プロジェクトはプロジェクトランドスケープ
+##This is project landscape.
+##このプロジェクトはプロジェクトランドスケープです。
+
+このプロジェクトの目標は論文「Topological data analysis of financial time series: Landscapes of crashes」の結果を再現することです。
+
+では、頑張りましょうか！！
---
+++ h.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 17 22:51:08 2018
+
+@author: cx513
+"""
+"""
+プロジェクトランドスケープ
+This project aims at reproduce results shown in paper:
+Topological data analysis of financial time series: Landscapes of crashes.
+"""
+"""
+Steps:
+    1.Get Data (Done)
+    2.Programming
+    3.Test
+    4.Get more data not in the paper
+
+Coding Steps:
+    Raw Data->Point Cloud(log return)->Persistent Diagrams
+    ->Persistence Landscapes->L^p norms
+"""
